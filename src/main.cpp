822238615ce3837edeb47f0f7b2a9fda01c9c97c
rayleigh18 <sitanggangirfan271000@gmail.com>
2021-06-07 14:21:26 +0700

    Change PIn

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,27 @@
 #include "getDataFromSerial.h"
 #include "HalBLDC/HalBLDC.h"
 
-#define HALL_PIN_A 21
-#define HALL_PIN_B 22
-#define HALL_PIN_C 23
+#define HALL_PIN_A 13
+#define HALL_PIN_B 12
+#define HALL_PIN_C 14
 
 // 138 for sepeda, 60 for motor di meja
 HalBLDC enc(HALL_PIN_A, HALL_PIN_B, HALL_PIN_C, 138);
+DriverBLDC motor(21,22,23, 0, 5000, 8);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   enc.setup();
+  motor.setup();
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.printf("%d %f\n",enc.getPulses(), enc.getRev());
+  recvWithStartEndMarkers();
+  parseData();
+  speed = (float)i/50;
+  
+  motor.inPWM(speed);
+  Serial.printf("%d %f %f\n",enc.getPulses(), enc.getRev(), speed);
+  delay(10);
 }
