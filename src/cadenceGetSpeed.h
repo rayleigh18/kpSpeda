3ebf8f975b9b1ab7e907562a55aee567362de706
rayleigh18 <sitanggangirfan271000@gmail.com>
2021-06-14 15:26:51 +0700

    Making another algorithm for control Creating upper limit for cadence speed

--- src/cadenceGetSpeed.h
+++ src/cadenceGetSpeed.h
@@ -8,6 +8,9 @@ void cadenceGetSpeed(){
     // }
     // else{
     rpm_cadence = cad.getPulses() * 1000 * 60 /((SAMP_CAD_MS - (millis() - last_time_cadence_intr))*12);
+    if (rpm_cadence > 500){
+        rpm_cadence = 500;
+    }
     // }
     #define CONST_FILTER_CADENCE 0.8
     rpm_cadence = CONST_FILTER_CADENCE*rpm_cadence + (1-CONST_FILTER_CADENCE)*last_rpm_cadence;
--- src/control.h
+++ src/control.h
@@ -1,8 +1,19 @@
 #ifndef CONTROL_H
 #define CONTROL_H
 #include "variable.h"
-#define KP 0.1
+#define KP 0.0001
 #define K_CAD 10
+#define MAX_PWM 0.4
+double fastPow(double a, double b) {
+    union {
+        double d;
+        int x[2];
+    } u = { a };
+    u.x[1] = (int)(b * (u.x[1] - 1072632447) + 1072632447);
+    u.x[0] = 0;
+    return u.d;
+}
+
 void control(){
     // if (speed_bike > 10  || speed_bike < 0){
     //   speed = 0;
@@ -23,13 +34,15 @@ void control(){
     */
 
     float rpm_motor_dest = K_CAD * rpm_cadence;
-    speed = KP * (rpm_motor_dest - rpm_motor);
+    // speed = KP * (rpm_motor_dest + rpm_motor);
 
-    if (speed < 0 || rpm_motor <= 20 || rpm_cadence <= 1){
+    speed = rpm_cadence*(1- fastPow(2.718281, -rpm_motor / 700)) / 100 + 0.03;
+
+    if (speed < 0 || rpm_motor <= 1.0 || rpm_cadence <= 1){
       speed = 0;
     }
-    if (speed > 0.2) {
-      speed = 0.2;
+    if (speed > MAX_PWM) {
+      speed = MAX_PWM;
     }
 }
 
