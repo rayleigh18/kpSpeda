00a25a1794f99c17d2fe8c1b800245a4eaa81fbe
rayleigh18 <sitanggangirfan271000@gmail.com>
2021-06-25 11:11:27 +0700

    Add enc get speed

---
+++ src/encGetSpeed.h
@@ -0,0 +1,15 @@
+#ifndef ENCGETSPEED_H
+#define ENCGETSPEED_H
+
+#include "variable.h"
+
+void encGetSpeed(){
+    rpm_motor  = enc.getRev() * 1000 * 60 / SAMP_ENC_MS;
+    enc.reset();
+    rpm_motor = 0.9*rpm_motor + 0.1 * rpm_last_motor;
+    accel_motor = (rpm_motor - rpm_last_motor)*1000/SAMP_ENC_MS;
+    rpm_last_motor = rpm_motor;
+    speed_bike = rpm_motor * R_RODA*60 /1000;
+}
+
+#endif
